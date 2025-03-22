c9736b4ddbc4f037c314d7868f24ec61e5ac0294
MikiTsa <mihail.trajkoski@student.um.si>
2025-03-22 18:08:13 +0100

    Vaja 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,28 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SendIt
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ContactsListView_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (contactsListView.SelectedItem != null)
+            {
+                string contactName = ((ListViewItem)contactsListView.SelectedItem).Content.ToString();
+                MessageBox.Show($"Selected contact: {contactName}", "Contact Selected", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
