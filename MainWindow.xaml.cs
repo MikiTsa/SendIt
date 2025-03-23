2082169555441f745ff07d27324e352fbfd7dba2
MikiTsa <mihail.trajkoski@student.um.si>
2025-03-23 18:00:06 +0100

    Vaja 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using SendIt.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -6,9 +9,27 @@ namespace SendIt
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Contact> Contacts { get; set; }
+        private Contact _selectedContact;
+
+        public Contact SelectedContact
+        {
+            get { return _selectedContact; }
+            set { _selectedContact = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Contacts = new ObservableCollection<Contact>
+            {
+                new Contact("Contact 1", "Online", "/Images/avatar1.png", "contact1@example.com", "+1 234 567 890", DateTime.Now.AddMinutes(-5)),
+                new Contact("Contact 2", "Away", "/Images/avatar2.png", "contact2@example.com", "+1 234 567 891", DateTime.Now.AddHours(-1)),
+                new Contact("Contact 3", "Busy", "/Images/avatar3.png", "contact3@example.com", "+1 234 567 892", DateTime.Now.AddHours(-3))
+            };
+
+            DataContext = this;
         }
 
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
@@ -20,8 +41,25 @@ namespace SendIt
         {
             if (contactsListView.SelectedItem != null)
             {
-                string contactName = ((ListViewItem)contactsListView.SelectedItem).Content.ToString();
-                MessageBox.Show($"Selected contact: {contactName}", "Contact Selected", MessageBoxButton.OK, MessageBoxImage.Information);
+                Contact selectedContact = (Contact)contactsListView.SelectedItem;
+                MessageBox.Show($"Selected contact: {selectedContact.Nickname}", "Contact Selected", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void ChangeStatus_Click(object sender, RoutedEventArgs e)
+        {
+            if (contactsListView.SelectedItem != null)
+            {
+                Contact selectedContact = (Contact)contactsListView.SelectedItem;
+
+                selectedContact.Status = selectedContact.Status switch
+                {
+                    "Online" => "Away",
+                    "Away" => "Busy",
+                    "Busy" => "Invisible",
+                    "Invisible" => "Online",
+                    _ => "Online"
+                };
             }
         }
     }
---
+++ Models/Contacts.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SendIt.Models
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        private string _nickname;
+        private string _status;
+        private BitmapImage _avatar;
+        private string _email;
+        private string _phoneNumber;
+        private DateTime _lastSeen;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Nickname
+        {
+            get { return _nickname; }
+            set
+            {
+                _nickname = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nickname)));
+            }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsOnline)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StatusColor)));
+            }
+        }
+
+        public BitmapImage Avatar
+        {
+            get { return _avatar; }
+            set
+            {
+                _avatar = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Avatar)));
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Email)));
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                _phoneNumber = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PhoneNumber)));
+            }
+        }
+
+        public DateTime LastSeen
+        {
+            get { return _lastSeen; }
+            set
+            {
+                _lastSeen = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastSeen)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastSeenFormatted)));
+            }
+        }
+
+        public bool IsOnline => Status == "Online";
+
+        public string LastSeenFormatted => LastSeen.ToString("g");
+
+        public Brush StatusColor
+        {
+            get
+            {
+                return Status switch
+                {
+                    "Online" => Brushes.Green,
+                    "Away" => Brushes.Orange,
+                    "Busy" => Brushes.Red,
+                    "Invisible" => Brushes.Gray,
+                    _ => Brushes.Gray
+                };
+            }
+        }
+
+        public Contact(string nickname, string status, string avatarPath, string email, string phoneNumber, DateTime lastSeen)
+        {
+            Nickname = nickname;
+            Status = status;
+            Avatar = new BitmapImage(new Uri(avatarPath, UriKind.Relative));
+            Email = email;
+            PhoneNumber = phoneNumber;
+            LastSeen = lastSeen;
+        }
+    }
+}
