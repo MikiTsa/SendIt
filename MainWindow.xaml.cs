c6ead81976135cc25c7a6aabe63f61d6dd16f557
MikiTsa <mihail.trajkoski@student.um.si>
2025-05-19 18:23:12 +0200

    Vaja 8

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,16 @@
 ﻿using SendIt.ViewModels;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Text;
 
 namespace SendIt
 {
     public partial class MainWindow : Window
     {
         public MainViewModel ViewModel { get; set; }
+        private SpeechManager _speechManager;
 
         public MainWindow()
         {
@@ -15,6 +18,37 @@ namespace SendIt
 
             ViewModel = new MainViewModel();
             DataContext = ViewModel;
+
+            _speechManager = new SpeechManager();
+
+            _speechManager.OpenAddContact += SpeechManager_OpenAddContact;
+            _speechManager.AddDefaultContact += SpeechManager_AddDefaultContact;
+            _speechManager.SelectContact += SpeechManager_SelectContact;
+            _speechManager.RemoveContact += SpeechManager_RemoveContact;
+            _speechManager.EditContact += SpeechManager_EditContact;
+            _speechManager.SendMessage += SpeechManager_SendMessage;
+            _speechManager.ChangeStatus += SpeechManager_ChangeStatus;
+
+            UpdateAvailableCommands();
+
+            ViewModel.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == nameof(ViewModel.SelectedContact) ||
+                    e.PropertyName == nameof(ViewModel.CanEditOrRemove))
+                {
+                    UpdateAvailableCommands();
+                }
+            };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateAvailableCommands();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _speechManager.Dispose();
         }
 
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
@@ -46,6 +80,141 @@ namespace SendIt
                     _ => "Online"
                 };
             }
+        }
+
+        private void ActivateSpeechMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            _speechManager.StartListening();
+            menuActivateSpeech.IsEnabled = false;
+            menuDeactivateSpeech.IsEnabled = true;
+            txtSpeechStatus.Text = "Voice commands active";
+        }
+
+        private void DeactivateSpeechMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            _speechManager.StopListening();
+            menuActivateSpeech.IsEnabled = true;
+            menuDeactivateSpeech.IsEnabled = false;
+            txtSpeechStatus.Text = "Voice commands inactive";
+        }
+
+        private void UpdateAvailableCommands()
+        {
+            _speechManager.UpdateAvailableCommands(ViewModel.CanEditOrRemove);
+
+            var commandsText = new StringBuilder("Available commands: ");
+            foreach (var command in _speechManager.AvailableCommands)
+            {
+                commandsText.Append($"'{command}', ");
+            }
+
+            if (_speechManager.AvailableCommands.Count > 0)
+            {
+                commandsText.Length -= 2;
+            }
+
+            txtAvailableCommands.Text = commandsText.ToString();
+        }
+
+        private void SpeechManager_OpenAddContact(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ViewModel.AddContactCommand.Execute(null);
+            });
+        }
+
+        private void SpeechManager_AddDefaultContact(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ViewModel.AddStaticContactCommand.Execute(null);
+                _speechManager.Speak("Default contact has been added to your list");
+            });
+        }
+
+        private void SpeechManager_SelectContact(object sender, string indexStr)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (int.TryParse(indexStr, out int index) && index >= 0 && index < ViewModel.Contacts.Count)
+                {
+                    contactsListView.SelectedIndex = index;
+                    contactsListView.ScrollIntoView(contactsListView.SelectedItem);
+                    _speechManager.Speak($"Selected contact {ViewModel.Contacts[index].Nickname}");
+                }
+                else
+                {
+                    _speechManager.Speak("Could not select the specified contact");
+                }
+            });
+        }
+
+        private void SpeechManager_RemoveContact(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (ViewModel.CanEditOrRemove)
+                {
+                    string contactName = ViewModel.SelectedContact.Nickname;
+                    ViewModel.RemoveContactCommand.Execute(null);
+                    _speechManager.Speak($"Contact {contactName} has been removed");
+                }
+                else
+                {
+                    _speechManager.Speak("Please select a contact first");
+                }
+            });
+        }
+
+        private void SpeechManager_EditContact(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (ViewModel.CanEditOrRemove)
+                {
+                    ViewModel.EditContactCommand.Execute(null);
+                }
+                else
+                {
+                    _speechManager.Speak("Please select a contact first");
+                }
+            });
+        }
+
+        private void SpeechManager_SendMessage(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (ViewModel.CanEditOrRemove)
+                {
+                    ViewModel.MessageText = "Hello from voice command!";
+                    ViewModel.SendMessageCommand.Execute(null);
+                    _speechManager.Speak("Message sent successfully");
+                }
+                else
+                {
+                    _speechManager.Speak("Please select a contact to send a message");
+                }
+            });
+        }
+
+        private void SpeechManager_ChangeStatus(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (ViewModel.CanEditOrRemove)
+                {
+                    string oldStatus = ViewModel.SelectedContact.Status;
+                    ChangeStatus_Click(null, null);
+                    string newStatus = ViewModel.SelectedContact.Status;
+                    _speechManager.Speak($"Status changed from {oldStatus} to {newStatus}");
+                }
+                else
+                {
+                    _speechManager.Speak("Please select a contact first");
+                }
+            });
         }
     }
 }
---
+++ SpeechManager.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows;
+
+namespace SendIt
+{
+    public class SpeechManager
+    {
+        private SpeechRecognitionEngine _recognizer;
+        private SpeechSynthesizer _synthesizer;
+        private bool _isListening;
+
+        public event EventHandler OpenAddContact;
+        public event EventHandler AddDefaultContact;
+        public event EventHandler<string> SelectContact;
+        public event EventHandler RemoveContact;
+        public event EventHandler EditContact;
+        public event EventHandler SendMessage;
+        public event EventHandler ChangeStatus;
+
+        private List<string> _availableCommands = new List<string>();
+        public List<string> AvailableCommands => _availableCommands;
+
+        public SpeechManager()
+        {
+            InitializeSpeechRecognition();
+            InitializeSpeechSynthesis();
+        }
+
+        private void InitializeSpeechRecognition()
+        {
+            try
+            {
+                _recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+
+                var choices = new Choices();
+                choices.Add("open add contact");
+                choices.Add("add default contact");
+                choices.Add("select contact one");
+                choices.Add("select contact two");
+                choices.Add("select contact three");
+                choices.Add("select first contact");
+                choices.Add("select second contact");
+                choices.Add("select third contact");
+                choices.Add("remove contact");
+                choices.Add("delete contact");
+                choices.Add("edit contact");
+                choices.Add("modify contact");
+                choices.Add("send message");
+                choices.Add("send hello");
+                choices.Add("change status");
+
+                var gb = new GrammarBuilder();
+                gb.Append(choices);
+                var grammar = new Grammar(gb);
+
+                _recognizer.LoadGrammar(grammar);
+                _recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+                _recognizer.SetInputToDefaultAudioDevice();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing speech recognition: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void InitializeSpeechSynthesis()
+        {
+            try
+            {
+                _synthesizer = new SpeechSynthesizer();
+                _synthesizer.SetOutputToDefaultAudioDevice();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing speech synthesis: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Confidence < 0.4)
+                return;
+
+            string command = e.Result.Text.ToLower();
+
+            switch (command)
+            {
+                case "open add contact":
+                    Speak("Opening add contact window");
+                    OpenAddContact?.Invoke(this, EventArgs.Empty);
+                    break;
+
+                case "add default contact":
+                    Speak("Adding a default contact");
+                    AddDefaultContact?.Invoke(this, EventArgs.Empty);
+                    break;
+
+                case "select contact one":
+                case "select first contact":
+                    Speak("First contact selected");
+                    SelectContact?.Invoke(this, "0");
+                    break;
+
+                case "select contact two":
+                case "select second contact":
+                    Speak("Second contact selected");
+                    SelectContact?.Invoke(this, "1");
+                    break;
+
+                case "select contact three":
+                case "select third contact":
+                    Speak("Third contact selected");
+                    SelectContact?.Invoke(this, "2");
+                    break;
+
+                case "remove contact":
+                case "delete contact":
+                    Speak("Contact has been removed");
+                    RemoveContact?.Invoke(this, EventArgs.Empty);
+                    break;
+
+                case "edit contact":
+                case "modify contact":
+                    Speak("Opening edit contact window");
+                    EditContact?.Invoke(this, EventArgs.Empty);
+                    break;
+
+                case "send message":
+                    Speak("Message sent");
+                    SendMessage?.Invoke(this, EventArgs.Empty);
+                    break;
+
+                case "send hello":
+                    Speak("Hello message sent");
+                    SendMessage?.Invoke(this, EventArgs.Empty);
+                    break;
+
+                case "change status":
+                    Speak("Status has been changed");
+                    ChangeStatus?.Invoke(this, EventArgs.Empty);
+                    break;
+            }
+        }
+
+        public void Speak(string message)
+        {
+            try
+            {
+                _synthesizer.SpeakAsync(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error in speech synthesis: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void StartListening()
+        {
+            if (!_isListening)
+            {
+                try
+                {
+                    Speak("Voice commands activated");
+                    _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                    _isListening = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error starting speech recognition: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        public void StopListening()
+        {
+            if (_isListening)
+            {
+                try
+                {
+                    Speak("Voice commands deactivated");
+                    _recognizer.RecognizeAsyncStop();
+                    _isListening = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error stopping speech recognition: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        public void UpdateAvailableCommands(bool canEditOrRemove)
+        {
+            _availableCommands.Clear();
+
+            _availableCommands.Add("open add contact");
+            _availableCommands.Add("add default contact");
+            _availableCommands.Add("select first contact");
+            _availableCommands.Add("select second contact");
+            _availableCommands.Add("select third contact");
+
+            if (canEditOrRemove)
+            {
+                _availableCommands.Add("remove contact");
+                _availableCommands.Add("edit contact");
+                _availableCommands.Add("send message");
+                _availableCommands.Add("change status");
+            }
+        }
+
+        public void Dispose()
+        {
+            StopListening();
+            _recognizer?.Dispose();
+            _synthesizer?.Dispose();
+        }
+    }
+}
